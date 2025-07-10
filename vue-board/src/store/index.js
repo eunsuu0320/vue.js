17f29b5daa193d037bdc716f704a7981df9ab970
choi eunsu <zxzx320@naver.com>
2025-07-11 00:22:28 +0900

    0710 공부안함

--- vue-board/src/store/index.js
+++ vue-board/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore } from "vuex";
 
+// createStore() 함수로 이루어진 스토어(전역 상태 관리소)를 만들고, 구성 정의
 export default createStore({
+  // 데이터 저장소
   state: {
+// posts 라는 배열 저장. => 게시글 목록을 저장하는 역할
     posts: [],
   },
+  // state에 정의된 변수를 변경시키는 역할. 동기 처리 commit을 통해서 호출, state 정의된 변수 값 변경 가능
   mutations: {
     add_post(state, post) {
       state.posts.push(post);
